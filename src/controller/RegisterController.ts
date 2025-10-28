514197aaf9ce06ea203448bad4a962b9b9cea813
Larigm <larissa.gmachado05@gmail.com>
2025-10-28 19:26:13 -0300

    registercontroller

--- src/controller/RegisterController.ts
+++ src/controller/RegisterController.ts
@@ -1,36 +1,22 @@
+import { Register } from "../model/Register";
 import { User } from "../model/User";
-import { Academic } from "../model/Academic";
-import { Register } from '../model/Register';
-import { Event } from '../model/Event';
-import { EventController } from './EventController';
+import { EventController } from "./EventController";
+
 
 export class RegisterController {
     private registers: Register[] = [];
     private eventController: EventController;
 
-    constructor(eventController: EventController) {
-        this.eventController = eventController;
+    constructor(eventController: EventController){
+        this.eventController = eventController; //injeção de dependencia
     }
+}
+    const eventController = new EventController();
+    const registerController = new RegisterController(EventController);
 
-    public inscreverUsuario(eventId: number, user: User): boolean {
+    public registerUser(eventId: number, user: User): boolean{
         const evento = this.eventController.findEvent(eventId);
         if (!evento) {
             return false;
-        }
 
-        const jaInscrito = evento.participantes.find(p => p.id === user.id);
-        if (jaInscrito) {
-            return false;
-        }
-        if (user instanceof Academic && evento.cursos.length > 0) {
-            
-            if (!evento.cursos.includes(user.course)) {
-                return false;
-            }
-        }
-
-        evento.adicionarParticipante(user);
-        this.registers.push(new Register(user, evento));
-        return true;
     }
-}
